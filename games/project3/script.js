2d22219bbd6babce18db0a809f33baeb435cd212
maxim <maxim526@gmail.com>
2023-07-18 02:10:04 +0300

    +1

---
+++ games/project3/script.js
@@ -0,0 +1,33 @@
+function signup() {
+  const obj = {
+    fullName: document.querySelector("#fullName").value,
+    email: document.querySelector("#email").value,
+    userName: document.querySelector("#userName").value,
+    password: document.querySelector("input[type=password]").value,
+  };
+
+  //   loader(true);
+
+  // שליחה לשרת
+  fetch("https://api.shipap.co.il/signup", {
+    method: "POST",
+    credentials: "include", // מאפשר שליחה וקבלה של עוגיות
+    headers: {
+      "Content-Type": "application/json", // הגדרת סוג התוכן הנשלח לשרת
+    },
+    body: JSON.stringify(obj), // תוכן הקריאה לשרת
+  })
+    // קבלה מהשרת
+    // *המרת התוכן לפי הצורך*
+    .then((res) => res.json())
+    // התוכן שהתקבל מהשרת (לאחר טיפול של הפונקציה הקודמת)
+    .then((data) => {
+      if (data.status == "success") {
+        setUser(data.user);
+        snackbar("המשתמש התחבר בהצלחה");
+      } else {
+        alert(data.message);
+        loader(false);
+      }
+    });
+}
